40dc6da3296bec77db37111cb8df528155e27718
evan <841978494@qq.com>
2021-08-20 22:26:37 +0800

    add all

---
+++ src/main/java/com/yb/chemical/ChemicalApplication.java
@@ -0,0 +1,13 @@
+package com.yb.chemical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChemicalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yb/chemical/ChemicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yb.chemical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
